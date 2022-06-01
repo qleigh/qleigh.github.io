6e9334e86a7e84f8e63bc62b5992bd5527eee9e6
qleigh <quiana.sison@student.unimelb.edu.au>
2022-06-01 22:30:19 +1000

    implement configuration options

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Menu functions
 function closeNav() {
     document.getElementById("this-menu-one").style.width = "0";
     document.getElementById("this-menu-two").style.width = "0";
@@ -19,4 +20,42 @@ function openNavTwo() {
         document.getElementById("this-menu-two").style.width = "100vw";
         document.getElementById("this-close-menu-two").style.height = "1.5em";
     } 
+}
+
+// configuration functions
+function openConfig(){
+    document.getElementById("collapsed-config").style.display = "none";
+    document.getElementById("expanded-config").style.display = "block";
+}
+
+function closeConfig(){
+    document.getElementById("collapsed-config").style.display = "block";
+    document.getElementById("expanded-config").style.display = "none";
+}
+
+currColour = 0;
+function selectLynxColour(option){
+    if (currColour != 0){
+        document.getElementById(currColour).style.border = "none";
+    }
+    currColour = option;
+    document.getElementById(option).style.border = "4px solid var(--colour-main)";
+}
+
+currConfig = 0;
+function selectLynxConfig(option){
+    if (currConfig != 0){
+        document.getElementById(currConfig).style.border = "none";
+    }
+    currConfig = option;
+    document.getElementById(option).style.border = "4px solid var(--colour-main)";
+}
+
+currStyle = 0;
+function selectChaseStyle(option){
+    if (currStyle != 0){
+        document.getElementById(currStyle).style.border = "none";
+    }
+    currStyle = option;
+    document.getElementById(option).style.border = "4px solid var(--colour-main)";
 }
